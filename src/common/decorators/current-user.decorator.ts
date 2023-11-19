9236d3a6abf140e6cb1d23d788a0cde470db31d2
Maksym-Holovachuk <mgolovachuk@gmail.com>
2023-11-19 20:34:35 +0200

    added decorator for get current user by jwt key

---
+++ src/common/decorators/current-user.decorator.ts
@@ -0,0 +1,11 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+import { IJwtPayload } from '@modules/auth/interfaces';
+export const CurrentUser = createParamDecorator(
+  (
+    key: keyof IJwtPayload,
+    ctx: ExecutionContext
+  ): IJwtPayload | Partial<IJwtPayload> => {
+    const request = ctx.switchToHttp().getRequest();
+    return key ? request.user[key] : request.user;
+  }
+);
--- src/common/decorators/index.ts
+++ src/common/decorators/index.ts
@@ -1,4 +1,5 @@
 export * from './is-passwords-matching-constraint.decorator';
 export * from './cookies.decorator';
 export * from './user-agent.decorator';
-export * from './skip-auth.decorator'
+export * from './skip-auth.decorator';
+export * from './current-user.decorator';
